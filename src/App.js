732a29f3495cdb378fb8f657b91466a28bbaae2f
coderboyakash <akash.chowdhury@weavers-web.com>
2022-05-22 16:05:26 +0530

    mentions working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,132 @@
-import React, { useMemo } from 'react'
-import { Transforms, createEditor, Descendant } from 'slate'
-import {Slate,Editable,useSlateStatic,useSelected,useFocused,withReact,ReactEditor} from 'slate-react'
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import { Transforms, createEditor, Editor, Range } from 'slate'
+import {Slate,Editable,useSlateStatic,useFocused,withReact,ReactEditor} from 'slate-react'
 import { withHistory } from 'slate-history'
 import imageExtensions from 'image-extensions'
 import isUrl from 'is-url'
 import Element from './Element'
+import Portal from './Portal'
 const App = () => {
-  const editor = useMemo(() => withImages(withHistory(withReact(createEditor()))),[])
+  const ref = useRef()
+  const [target, setTarget] = useState()
+  const [index, setIndex] = useState(0)
+  const [search, setSearch] = useState('')
+  const editor = useMemo(() => withMentions(withImages(withHistory(withReact(createEditor())))),[])
   const initialValue = [
     {
       type: 'paragraph',
       children: [{text: ''}]
     }
   ];
+  const onKeyDown = useCallback(
+    event => {
+      if (target) {
+        switch (event.key) {
+          case 'ArrowDown':
+            event.preventDefault()
+            const prevIndex = index >= chars.length - 1 ? 0 : index + 1
+            setIndex(prevIndex)
+            break
+          case 'ArrowUp':
+            event.preventDefault()
+            const nextIndex = index <= 0 ? chars.length - 1 : index - 1
+            setIndex(nextIndex)
+            break
+          case 'Tab':
+          case 'Enter':
+            event.preventDefault()
+            Transforms.select(editor, target)
+            insertMention(editor, chars[index])
+            setTarget(null)
+            break
+          case 'Escape':
+            event.preventDefault()
+            setTarget(null)
+            break
+        }
+      }
+    },
+    [index, search, target]
+  )
+  const chars = CHARACTERS.filter(c =>
+    c.toLowerCase().startsWith(search.toLowerCase())
+  ).slice(0, 10)
+  useEffect(() => {
+    if (target && chars.length > 0) {
+      const el = ref.current
+      const domRange = ReactEditor.toDOMRange(editor, target)
+      const rect = domRange.getBoundingClientRect()
+      el.style.top = `${rect.top + window.pageYOffset + 24}px`
+      el.style.left = `${rect.left + window.pageXOffset}px`
+    }
+  }, [chars.length, editor, index, search, target])
   return (
-    <Slate editor={editor} value={initialValue}>
+    <Slate 
+      editor={editor} 
+      value={initialValue}
+      onChange={() => {
+        const { selection } = editor
+
+        if (selection && Range.isCollapsed(selection)) {
+          const [start] = Range.edges(selection)
+          const wordBefore = Editor.before(editor, start, { unit: 'word' })
+          const before = wordBefore && Editor.before(editor, wordBefore)
+          const beforeRange = before && Editor.range(editor, before, start)
+          const beforeText = beforeRange && Editor.string(editor, beforeRange)
+          const beforeMatch = beforeText && beforeText.match(/^@(\w+)$/)
+          const after = Editor.after(editor, start)
+          const afterRange = Editor.range(editor, start, after)
+          const afterText = Editor.string(editor, afterRange)
+          const afterMatch = afterText.match(/^(\s|$)/)
+
+          if (beforeMatch && afterMatch) {
+            setTarget(beforeRange)
+            setSearch(beforeMatch[1])
+            setIndex(0)
+            return
+          }
+        }
+
+        setTarget(null)
+      }}
+      >
       <Editable
         renderElement={props => <Element {...props} />}
         placeholder="Enter some text..."
         className='slate-editor'
+        onKeyDown={onKeyDown}
       />
+      {target && chars.length > 0 && (
+        <Portal>
+          <div
+            ref={ref}
+            style={{
+              top: '-9999px',
+              left: '-9999px',
+              position: 'absolute',
+              zIndex: 1,
+              padding: '3px',
+              background: 'white',
+              borderRadius: '4px',
+              boxShadow: '0 1px 5px rgba(0,0,0,.2)',
+            }}
+            data-cy="mentions-portal"
+          >
+            {chars.map((char, i) => (
+              <div
+                key={char}
+                style={{
+                  padding: '1px 3px',
+                  borderRadius: '3px',
+                  background: i === index ? '#B4D5FF' : 'transparent',
+                }}
+              >
+                {char}
+              </div>
+            ))}
+          </div>
+        </Portal>
+      )}
       <InsertImageButton />
     </Slate>
   )
@@ -60,6 +167,19 @@ const withImages = editor => {
 
   return editor
 }
+const withMentions = editor => {
+  const { isInline, isVoid } = editor
+
+  editor.isInline = element => {
+    return element.type === 'mention' ? true : isInline(element)
+  }
+
+  editor.isVoid = element => {
+    return element.type === 'mention' ? true : isVoid(element)
+  }
+
+  return editor
+}
 const InsertImageButton = () => {
   const editor = useSlateStatic()
   return (
@@ -80,10 +200,24 @@ const insertImage = (editor, url) => {
   Transforms.insertNodes(editor, image)
 }
 
+const insertMention = (editor, character) => {
+  const mention = {
+    type: 'mention',
+    character,
+    children: [{ text: '' }],
+  }
+  Transforms.insertNodes(editor, mention)
+  Transforms.move(editor)
+}
+
 const isImageUrl = url => {
   if (!url) return false
   if (!isUrl(url)) return false
   const ext = new URL(url).pathname.split('.').pop()
   return imageExtensions.includes(ext)
 }
+const CHARACTERS = [
+  'Aayla Secura',
+  'Adi Gallia'
+]
 export default App
--- src/Element.js
+++ src/Element.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Image from './Image'
+import Mention from './Mention'
 const Element = props => {
   const { attributes, children, element } = props
   switch (element.type) {
     case 'image':
       return <Image {...props} />
+    case 'mention':
+      return <Mention {...props} />
     default:
       return <div {...attributes}>{children}</div>
   }
